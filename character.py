7799366b597da5c06bd3d36e7bb9c927f5c8ef27
samupti <uptigrovesam@gmail.com>
2024-07-01 17:21:14 -0400

    character
    ..

--- character.py
+++ character.py
@@ -1,3 +1,6 @@
-#hello
-#sucky pucky
-#testing
+from abc import ABC, abstractmethod
+from item import Item, Armor, Weapon
+
+class Character(ABC):
+    def __init__(self, name: str, health: list[int], mana: list[int], physical_stats: list[int], magical_stats: list[int], luck: int, critical_percent: int, critical_modifier: float, inventory: list[Item], equipment: dict[str, Armor], weapon: Weapon ):
+        self.__name =
