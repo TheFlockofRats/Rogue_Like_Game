cd4e6407ee76ab4e3ee91c306170c0745486a23d
Aaron-Roe <144751185+Aaron-Roe@users.noreply.github.com>
2024-07-10 06:47:32 -0700

    more docs

--- character.py
+++ character.py
@@ -4,7 +4,9 @@ from math import floor
 from item import Item, Armor, Weapon, Condition, AttackType
 import random
 
-
+'''
+CHARACTER attack(self, target: Character) -> str's if critical_strike_bool is True: the else statement does not make sense as its in the crit True section and will never get hit.
+'''
 class Character(ABC):
     """ A class that creates the character's for the game
     ...
@@ -297,21 +299,27 @@ class Character(ABC):
         d = Weapon.damage
         damage = random.randint(1, d)
         rand_num = random.randint(1, 100)
+        # Check to ensure that the rand_num is less than the self.__critical_percentage added to self.__luck
         if rand_num <= (self.__critical_percentage + self.__luck):
             damage = floor(damage * self.__critical_modifier)
             critical_strike_bool = True
         else:
             critical_strike_bool = False
+        # returns damage, and the bool for if there was a critical strike
         return damage, critical_strike_bool
 
     def take_damage(self, damage) -> None:
+        #  This method subtracts damage input from the temporary health of the character
         temp_health = damage - self.__health[0]
+        # Check to see if temp_health is less than 1 if true raise the CharacterDeathException
         if temp_health < 1:
             raise CharacterDeathException
 
     def attack(self, target: Character, damage, critical_strike_bool) -> str:
+        # This method determines if a character can attack a Creature. If it can, the character's damage is determined and dealt.
         attacker = random.randint(1, 20)
         defender = random.randint(1, 20)
+        # Check to see if weapon attack type is 1 and setting stats if True
         if Weapon.attack_type == 1:
             attacker += Armor.physical_attack
             attacker += Armor.physical_defense
@@ -320,6 +328,7 @@ class Character(ABC):
             defender += Armor.physical_defense
 
         elif Weapon.attack_type == 2:
+            # Check to see if weapon attack type is 2 and setting stats if True
             attacker += Armor.magical_attack
             attacker += Armor.magical_defense
 
@@ -327,30 +336,41 @@ class Character(ABC):
             defender += Armor.magical_defense
 
         if critical_strike_bool is True:
-
+            # check to see if critical_strike_bool is True
             if attacker >= defender:
+                # Check to see if the attackers attack damage is greater than the defenders defense
                 target.deal_damage()
                 if target.health[0] <= 0:
+                    # Check to see if target's dammage
                     return f'{target} lost {damage} health and died with a critical hit from {self.__name}!'
                 else:
                     return f'{target} lost {damage} health with a critical hit from {self.__name}!'
 
             else:
+# Need to fix the else here
                 if target.health[0] <= 0:
                     return f'{target} lost {damage} health and died from {self.__name}!'
                 else:
                     return f'{target} lost {damage} health from {self.__name}!'
 
     def equip(self, item: Item, position: str = None) -> None:
+        """
+        This method passes in an Item that is either Armor or a Weapon and a position on the body to place the Item.
+        If the Item is a Weapon a position is not needed and simply place the previous Weapon into the Character's __inventory and set the __weapon to the item.
+        If the Item is Armor, replace the Item at the position with the item and place the previous Item into the Character's __inventory.
+        """
+        # Check if the Item is not Armor or a Weapon or the position is not in __equipment then raises CannotEquipException
         if not isinstance(item, Weapon) or not isinstance(item, Armor):
             raise CannotEquipException
 
+        # Check to see if item is a weapon if so places that weapon in inventory and sets self.__weapon to the item
         if isinstance(item, Weapon):
             self.__inventory.append(self.__weapon)
             self.__weapon = item
 
+        # Check to see if item is Armor
         elif isinstance(item, Armor):
-
+            # Checks to see if the position is one of the acceptable options and placing the armor at that location if possible
             if position == 'HEAD':
                 self.__inventory.append(self.__equipment[0])
                 self.__equipment[0] = item
@@ -370,21 +390,25 @@ class Character(ABC):
             elif position == 'FEET':
                 self.__inventory.append(self.__equipment[4])
                 self.__equipment[4] = item
-
+            # If not possible to place the item raise CannotEquipException
             else:
                 raise CannotEquipException
 
     def pick_up(self, item: Item) -> str:
+        # Put's the item into the Character's __inventory and returns a string to indicate it was picked up.
         self.__inventory.append(item)
         return f'{item} was picked up'
 
     def drop(self, item: Item) -> (str, Item):
+        # Removes an item from a Character's __inventory and returns it with a string to indicate that it was dropped.
         self.__inventory.remove(item)
         return f'{item} was dropped'
 
 
 class Creature(Character):
-
+    """
+    This Class was provided to us and will create a creature with a name and initialize its stats, health and armor
+    """
     def __init__(self, char_name):
         super().__init__(char_name)
         self.physical_stats = [random.randint(0, 8), random.randint(0, 8)]
@@ -398,6 +422,10 @@ class Creature(Character):
 
 
 class Warrior(Character):
+    """
+    The Warrior class is meant to create a fighter that specializes in physical combat.
+    They will have a greater amount of health, a greater physical defense, but little magical defense.
+    """
     def __init__(self, char_name: str):
         super().__init__(char_name)
 
@@ -416,10 +444,13 @@ class Warrior(Character):
         self.__magical_stats[1] -= rand_num_magical
 
     def karate_kick(self, target: Creature):
+        # One of the three aditions that we added, check to ensure that the target has enough mana then preforms the damage
         if self.__mana[0] >= 2:
             target.health[0] -= 3
             self.__mana[0] -= 2
+            # if enough mana returns a damage dealt text
             return f'{3} damage has been dealt to {target}'
+        # Not enough mana the attack failed
         else:
             raise LowMana('Karate Kid ran out of stamina because he wrote too much code for this project')
 
